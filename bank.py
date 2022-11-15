318a1ed1492dfb3ce932339d3027dc7fca89509d
edsonibarra <edsonibarra97@gmail.com>
2022-11-14 20:11:54 -0600

    bank

---
+++ bank.py
@@ -0,0 +1,10 @@
+class Bank:
+    def __init__(self, name, bank_code):
+        self.__name = name
+        self.__bank_code = bank_code
+
+    def get_bank_code(self):
+        return self.__bank_code
+
+    def add_atm(self, atm):
+        None
