c90f2cc6f6901eeb8306da4035635d8a137f71c1
edsonibarra <edsonibarra97@gmail.com>
2022-11-14 20:12:44 -0600

    cash dispenser

---
+++ cash_dispenser.py
@@ -0,0 +1,10 @@
+class CashDispenser:
+    def __init__(self):
+        self.__total_five_dollar_bills = 0
+        self.__total_twenty_dollar_bills = 0
+
+    def dispense_cash(self, amount):
+        None
+
+    def can_dispense_cash(self):
+        None
