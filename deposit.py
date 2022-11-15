be346eb4555dbc0f4e4b0a021b25c7fcd9e1ba0a
edsonibarra <edsonibarra97@gmail.com>
2022-11-14 20:13:33 -0600

    deposit

---
+++ deposit.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class DepositSlot(ABC):
+    def __init__(self):
+        self.__total_amount = 0.0
+
+    def get_total_amount(self):
+        return self.__total_amount
+
+
+class CheckDepositSlot(DepositSlot):
+    def get_check_amount(self):
+        None
+
+
+class CashDepositSlot(DepositSlot):
+    def receive_dollar_bill(self):
+        None
