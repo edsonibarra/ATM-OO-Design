13aae5c54f565deb52d48d58750cbd024772fe76
edsonibarra <edsonibarra97@gmail.com>
2022-11-14 20:11:18 -0600

    account

---
+++ account.py
@@ -0,0 +1,8 @@
+class Account:
+    def __init__(self, account_number):
+        self.__account_number = account_number
+        self.__total_balance = 0.0
+        self.__available_balance = 0.0
+
+    def get_available_balance(self):
+        return self.__available_balance
