de8a8645706f880757d484cb897fedd3498052c1
edsonibarra <edsonibarra97@gmail.com>
2022-11-14 20:12:12 -0600

    card

---
+++ card.py
@@ -0,0 +1,9 @@
+class Card:
+    def __init__(self, number, customer_name, expiry, pin):
+        self.__card_number = number
+        self.__customer_name = customer_name
+        self.__card_expiry = expiry
+        self.__pin = pin
+
+    def get_billing_address(self):
+        None
